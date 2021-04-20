960cd2ddaea8b974b2cefac2b5ff61bbe5ee7c7c
richard-j-craig <richard.j.craig@mail.com>
2021-04-20 21:11:28 +0100

    updated to OOP

---
+++ fee_sets.py
@@ -0,0 +1,17 @@
+
+fee_sets = {
+    "12": [
+        {"amount": 1000, "fee": 50},
+        {"amount": 2000, "fee": 90},
+        {"amount": 3000, "fee": 90},
+        {"amount": 4000, "fee": 115},
+        {"amount": 5000, "fee": 100},
+        {"amount": 20000, "fee": 400}
+    ],
+    "24": [
+        {"amount": 1000, "fee": 70},
+        {"amount": 2000, "fee": 100},
+        {"amount": 3000, "fee": 120},
+        {"amount": 20000, "fee": 800}       
+    ]
+}
---
+++ loan_application.py
@@ -0,0 +1,32 @@
+from enum import IntEnum
+
+from fee_sets import fee_sets
+
+
+class RepaymentOptions(IntEnum):
+    """Valid options for repayment periods (months)"""
+    plan_1 = 12
+    plan_2 = 24
+
+
+class LoanApplication:
+    def __init__(self, repay_period, amount):
+        self.repay_period = repay_period
+        self.amount = round(amount, 2)
+
+    def fee(self):
+        """Function to calculate fee for chosen loan amount and repayment plan"""
+        fee_set = fee_sets[str(self.repay_period)]
+        for i in range(1, len(fee_set)):
+            if fee_set[i-1]["amount"] <= self.amount <= fee_set[i]["amount"]:
+                # interpolate fee (linearly) from given interval
+                ratio = ((self.amount - fee_set[i-1]["amount"])
+                         / (fee_set[i]["amount"] - fee_set[i-1]["amount"]))
+                unaltered_fee = (ratio 
+                                 * (fee_set[i]["fee"] - fee_set[i-1]["fee"])
+                                 + fee_set[i-1]["fee"])
+                # alter fee so that fee + loan amount is a multiple of 5
+                rounded_total = round((self.amount + unaltered_fee) / 5) * 5
+                fee = round(rounded_total - self.amount, 2)
+                return fee
+            
